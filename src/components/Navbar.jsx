2a3302d7eb90cb678e3a0f3a1c3f4b80760fcd26
isnancahyadi <isnan.arifc@gmail.com>
2023-06-11 15:38:31 +0700

    [ADD] carousel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,120 +6,118 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <>
-      <div className="Navbar">
-        <div className="row align-items-center">
-          <div id="blanja-header" className="row align-items-center">
-            <div className="container">
-              <div className="row justify-content-around align-items-center">
-                <Link className="logo col-auto me-4" to={"/"}>
-                  <img
-                    src="/assets/logo_blanja.png"
-                    alt="logo-blanja"
-                    height={40}
-                  />
-                </Link>
-                <div className="main-nav col ms-4">
-                  <div className="row align-items-center d-flex">
-                    <div className="col search">
-                      <input
-                        type="text"
-                        className="form-control decoration-none"
-                        placeholder="Search Product"
-                      />
-                      <FontAwesomeIcon
-                        id="ic-search"
-                        className="ic"
-                        icon="magnifying-glass"
-                        size="lg"
-                      />
-                    </div>
-                    <div className="col-auto filters">
-                      <FontAwesomeIcon
-                        id="ic-filters"
-                        className="ic"
-                        icon="sliders"
-                        size="lg"
-                      />
-                    </div>
+    <div className="Navbar">
+      <div className="row align-items-center">
+        <div id="blanja-header" className="row align-items-center">
+          <div className="container">
+            <div className="row justify-content-around align-items-center">
+              <Link className="logo col-auto me-4" to={"/"}>
+                <img
+                  src="/assets/logo_blanja.png"
+                  alt="logo-blanja"
+                  height={40}
+                />
+              </Link>
+              <div className="main-nav col ms-4">
+                <div className="row align-items-center d-flex">
+                  <div className="col search">
+                    <input
+                      type="text"
+                      className="form-control decoration-none"
+                      placeholder="Search Product"
+                    />
+                    <FontAwesomeIcon
+                      id="ic-search"
+                      className="ic"
+                      icon="magnifying-glass"
+                      size="lg"
+                    />
+                  </div>
+                  <div className="col-auto filters">
+                    <FontAwesomeIcon
+                      id="ic-filters"
+                      className="ic"
+                      icon="sliders"
+                      size="lg"
+                    />
                   </div>
                 </div>
-                <div className="action col-auto">
-                  <div className="row align-items-center d-flex">
-                    <div className="col-auto me-3">
+              </div>
+              <div className="action col-auto">
+                <div className="row align-items-center d-flex">
+                  <div className="col-auto me-3">
+                    <FontAwesomeIcon
+                      id="ic-shopping-cart"
+                      className="ic"
+                      icon="shopping-cart"
+                      size="lg"
+                      style={{ cursor: "pointer" }}
+                    />
+                  </div>
+                  <div className="col-auto btn-login">
+                    <button
+                      type="button"
+                      class="btn btn-primary border-2 rounded-pill"
+                    >
+                      Login
+                    </button>
+                  </div>
+                  <div className="col-auto btn-regis">
+                    <button
+                      type="button"
+                      class="btn btn-light border-2 border rounded-pill"
+                    >
+                      Signup
+                    </button>
+                  </div>
+                  <div className="col-auto d-flex justify-content-between align-items-center menu">
+                    <button
+                      class="btn"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseExample"
+                      aria-expanded="false"
+                      aria-controls="collapseExample"
+                    >
                       <FontAwesomeIcon
-                        id="ic-shopping-cart"
-                        className="ic"
-                        icon="shopping-cart"
+                        icon="bars"
                         size="lg"
-                        style={{ cursor: "pointer" }}
+                        style={{ color: "white", cursor: "pointer" }}
                       />
-                    </div>
-                    <div className="col-auto btn-login">
-                      <button
-                        type="button"
-                        class="btn btn-primary border-2 rounded-pill"
-                      >
-                        Login
-                      </button>
-                    </div>
-                    <div className="col-auto btn-regis">
-                      <button
-                        type="button"
-                        class="btn btn-light border-2 border rounded-pill"
-                      >
-                        Signup
-                      </button>
-                    </div>
-                    <div className="col-auto d-flex justify-content-between align-items-center menu">
-                      <button
-                        class="btn"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#collapseExample"
-                        aria-expanded="false"
-                        aria-controls="collapseExample"
-                      >
-                        <FontAwesomeIcon
-                          icon="bars"
-                          size="lg"
-                          style={{ color: "white", cursor: "pointer" }}
-                        />
-                      </button>
-                    </div>
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div
-            class="row collapse justify-content-end text-end"
-            id="collapseExample"
-          >
-            <div class="card">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                  <Link
-                    className="text-black text-decoration-none mb-3 text-center"
-                    to="/"
-                  >
-                    Login
-                  </Link>
-                </li>
-                <li class="list-group-item">
-                  <Link
-                    className="text-black text-decoration-none mb-3 text-center"
-                    to="/"
-                  >
-                    Register
-                  </Link>
-                </li>
-              </ul>
-            </div>
+        </div>
+        <div
+          class="row collapse justify-content-end text-end"
+          id="collapseExample"
+        >
+          <div class="card">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <Link
+                  className="text-black text-decoration-none mb-3 text-center"
+                  to="/"
+                >
+                  Login
+                </Link>
+              </li>
+              <li class="list-group-item">
+                <Link
+                  className="text-black text-decoration-none mb-3 text-center"
+                  to="/"
+                >
+                  Register
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,24 @@ import "../style/pages/Dashboard.scss";
 import Navbar from "../components/Navbar";
 
 import React from "react";
+import CarouselHome from "../components/CarouselHome";
 
 function Dashboard() {
   return (
     <div className="Dashboard">
-      <Navbar />
+      <div className="row">
+        {/* <div className="row"> */}
+        <Navbar />
+        {/* </div> */}
+
+        <section id="carousel" className="container">
+          <div className="row">
+            <div className="col-12">
+              <CarouselHome />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
