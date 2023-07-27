5ae029440941d59e4fe53e644990f827497ef3a9
IrhamNfrnda <inofrianda@gmail.com>
2023-07-27 11:46:11 +0700

    [FIX] Resolve Conflict

--- src/pages/profile/MyOrder.jsx
+++ src/pages/profile/MyOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import Sidebar from "../../components/Sidebar";
 import Navbar from "../../components/Navbar";
 import "../../style/pages/MyOrder.scss";
